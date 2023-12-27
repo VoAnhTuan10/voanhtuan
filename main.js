916ca7d4d0a33dd6a3f99815d421d5b07a4ee04f
DESKTOP-F2H67QP\ADMIN <anhtuanvo10111997@gmail.com>
2023-12-27 22:52:35 +0700

    add file

---
+++ main.js
@@ -0,0 +1,39 @@
+
+// var fullName = ('Vo Anh Tuan')
+// console.log(fullName)
+// var a = 1;
+// var b = 2;
+// if(a<b){
+//     alert('dung');
+// }
+// var c = 15;
+// var d = 4;
+// var e = c % d;
+// console.log(e);
+
+
+// var age = 20;
+// var a = 1;
+// var nextAge = age + a;
+// console.log(nextAge);
+
+// var a = 4;
+// var b = 2;
+// b -= a;
+// console.log(b);
+// var c = a + b;
+// var d = a - b;
+// console.log(c,d);
+
+var age = 1;
+console.log(age++);//1
+console.log(age);//2
+console.log(age++);// trả về giá trị trước khi tăng vẫn = 2
+console.log(age);// 3
+
+
+var number = 4;
+console.log(--number); //4
+console.log(number);//3
+console.log(--number);//3
+console.log(number);//2
